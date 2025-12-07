8258cbaa00b6e6adff0d26b53ff716bf817e3767
jaken92 <jaaakeeen@gmail.com>
2025-12-07 19:29:37 +0100

    cart link + header styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -10,9 +10,9 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   const {shop, menu} = header;
   return (
     <header className="header">
-      <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
+      {/* <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
         <strong>{shop.name}</strong>
-      </NavLink>
+      </NavLink> */}
       <HeaderMenu
         menu={menu}
         viewport="desktop"
@@ -89,14 +89,14 @@ function HeaderCtas({isLoggedIn, cart}) {
   return (
     <nav className="header-ctas" role="navigation">
       <HeaderMenuMobileToggle />
-      <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
+      {/* <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
         <Suspense fallback="Sign in">
           <Await resolve={isLoggedIn} errorElement="Sign in">
             {(isLoggedIn) => (isLoggedIn ? 'Account' : 'Sign in')}
           </Await>
         </Suspense>
-      </NavLink>
-      <SearchToggle />
+      </NavLink> */}
+      {/* <SearchToggle /> */}
       <CartToggle cart={cart} />
     </nav>
   );
@@ -144,7 +144,25 @@ function CartBadge({count}) {
         });
       }}
     >
-      Cart {count === null ? <span>&nbsp;</span> : count}
+      <svg
+        className="cartSvg"
+        fill="none"
+        height="25"
+        viewBox="0 0 24 24"
+        width="25"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          className="cartSvg"
+          fill="black"
+          style={{transition: 'fill 0.3s ease-out'}}
+        >
+          <path d="m2.08416 2.7512c.13739-.39076.56554-.59617.95631-.45878l.3014.10596c.61652.21673 1.14016.40081 1.55224.60301.44063.2162.81848.48254 1.10266.8984.28198.41264.3984.86536.45213 1.36174.02405.2222.03674.46814.04343.73847h10.63817c1.685 0 3.2018 0 3.6457.57708.444.57709.2704 1.44661-.0767 3.18567l-.4998 2.42475c-.3151 1.5289-.4727 2.2933-1.0244 2.7429s-1.3322.4496-2.8932.4496h-5.3029c-2.78892 0-4.18337 0-5.04977-.9138-.86641-.9139-.92964-1.8846-.92964-4.8262v-2.60168c0-.73995-.00102-1.23516-.04218-1.61537-.03933-.36335-.10903-.54477-.19929-.67686-.08806-.12886-.22173-.24929-.52496-.39807-.32284-.15841-.76159-.31396-1.4292-.54868l-.26121-.09184c-.39077-.13738-.59617-.56553-.45879-.9563z" />
+          <path d="m7.5 18c.82843 0 1.5.6716 1.5 1.5s-.67157 1.5-1.5 1.5-1.5-.6716-1.5-1.5.67157-1.5 1.5-1.5z" />
+          <path d="m16.5 18.0001c.8284 0 1.5.6715 1.5 1.5 0 .8284-.6716 1.5-1.5 1.5s-1.5-.6716-1.5-1.5c0-.8285.6716-1.5 1.5-1.5z" />
+        </g>
+      </svg>
+      {count === null ? <span>&nbsp;</span> : count}
     </a>
   );
 }
@@ -219,7 +237,8 @@ const FALLBACK_HEADER_MENU = {
 function activeLinkStyle({isActive, isPending}) {
   return {
     fontWeight: isActive ? 'bold' : undefined,
-    color: isPending ? 'grey' : 'black',
+    color: '#3f4140',
+    // color: isPending ? 'grey' : 'black',
   };
 }
 
